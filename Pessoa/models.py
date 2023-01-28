cba9f5ff9aa8b4825ef3544b49d0dbe496628b67
mauro-jr@outlook.com <mauro-jr@outlook.com>
2023-01-28 16:07:31 -0300

    commit_mj_bd

--- Pessoa/models.py
+++ Pessoa/models.py
@@ -15,10 +15,14 @@ class Pessoa(models.Model):
         ('D', 'DEPRESSAO'),
     }
 
-    acontecimento_negativo = models.TextField('Acontecimento Negativo',max_length=2000)
-    acontecimento_positivo = models.TextField('Acontecimento Positivo',max_length=2000)
-    estado_mental =  models.CharField("Selecione o seu estado mental atual",choices=avaliacao_diaria)
-    alerta_crises = models.CharField("Selecione o seu estado de crise",choices=alerta_crises)
+    nome = models.CharField(max_length=100)
+    acontecimento_negativo = models.TextField('Acontecimento Negativo',max_length=100)
+    acontecimento_positivo = models.TextField('Acontecimento Positivo',max_length=100)
+    estado_mental =  models.CharField("Selecione o seu estado mental atual", max_length=2,choices=avaliacao_diaria)
+    alerta_crises = models.CharField("Selecione o seu estado de crise", max_length=2,choices=alerta_crises)
     data_atual = models.DateTimeField(auto_now=True)
     data_acontecimento = models.DateTimeField(auto_now=False)
     data_superaçao = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.nome
--- Pessoa/views.py
+++ Pessoa/views.py
@@ -5,9 +5,11 @@ from pessoa.models import Pessoa
 def listar_monitoramento(request):
 
     if request.method == 'GET':
-        registro = Pessoa.objects.all()
+        registros = Pessoa.objects.all()
 
-        return render(request,'home.html', {'registro': registro})
+        return render(request,'home.html', {'registros': registros})
+
+    
 
 
 
