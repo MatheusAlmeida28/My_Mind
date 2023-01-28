5f6701953ef5bcd747c8d71c047fcdc223431987
mauro-jr@outlook.com <mauro-jr@outlook.com>
2023-01-28 14:51:15 -0300

    commit_models_mj

--- Pessoa/models.py
+++ Pessoa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+
+    avaliacao_diaria = {
+        ('P', 'PÉSSIMO'),    
+        ('R', 'RUIM'),    
+        ('B', 'BOM'),
+        ('E', 'EXCELENTE'),
+        }
+
+
+    alerta_crises = {
+        ('P', 'PANICO'),    
+        ('A', 'ANSIEDADE'),    
+        ('D', 'DEPRESSAO'),
+    }
+
+    acontecimento_negativo = models.TextField('Acontecimento Negativo',max_length=100)
+    acontecimento_positivo = models.TextField('Acontecimento Positivo',max_length=100)
+    estado_mental =  models.CharField("Selecione o seu estado mental atual", max_length=30,choices=avaliacao_diaria)
+    alerta_crises = models.CharField("Selecione o seu estado de crise", max_length=30,choices=alerta_crises)
+    data_atual = models.DateTimeField(auto_now=True)
+    data_acontecimento = models.DateTimeField(auto_now=False)
+    data_superaçao = models.DateTimeField(auto_now=False)
