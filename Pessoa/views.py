f601de26176e5f440601f2243bf3d477e97cc626
mauro-jr@outlook.com <mauro-jr@outlook.com>
2023-01-28 15:41:51 -0300

    commit_mj_v3

--- Pessoa/views.py
+++ Pessoa/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from pessoa.models import Pessoa
 
 
-def home(request):
-    return render(request, 'home.html')
+def listar_monitoramento(request):
+
+    if request.method == 'GET':
+        registro = Pessoa.objects.all()
+
+        return render(request,'home.html', {'registro': registro})
+
+
+
+
+
